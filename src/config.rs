c39c7d477a2396689c3ccec1b1702f0f4ea47bfa
ipeglin <philip@eglin.no>
2024-07-15 22:35:12 +0200

    feat: reading config from toml and parse to config struct

--- src/config.rs
+++ src/config.rs
@@ -1,4 +1,5 @@
 use serde::Deserialize;
+use std::fs;
 
 #[destruct(Deserialize)]
 struct Config {
@@ -14,6 +15,6 @@ struct HardwareConfig {
 }
 
 pub fn load() -> Config {
-  let config_string = fs::read_to_string("config.toml").expect("Failed to load config file");
-  toml::from_string(config_string).expect("Failed to parse configuration from file")
+    let config_string = fs::read_to_string("config.toml").expect("Failed to load config file");
+    toml::from_string(config_string).expect("Failed to parse configuration from file")
 }
