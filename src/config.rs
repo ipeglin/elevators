927f8806de06d365240cbc65168023cc081c7285
ipeglin <philip@eglin.no>
2024-07-15 22:31:43 +0200

    feat: read config from toml and parse to struct

--- src/config.rs
+++ src/config.rs
@@ -12,3 +12,8 @@ struct HardwareConfig {
     DRIVER_PORT: u32,
     DRIVER_CHANNEL_POLL_TIMEOUT_MILLISECONDS: u32,
 }
+
+pub fn load() -> Config {
+  let config_string = fs::read_to_string("config.toml").expect("Failed to load config file");
+  toml::from_string(config_string).expect("Failed to parse configuration from file")
+}
