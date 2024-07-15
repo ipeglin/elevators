d129bbe3a313af70e618c8943adda4433a6a064b
ipeglin <philip@eglin.no>
2024-07-15 21:37:08 +0200

    feat: handling incoming messages in hardware driver

--- src/elevator/hardware.rs
+++ src/elevator/hardware.rs
@@ -1,13 +1,18 @@
+use std::time::Duration;
+
 use crossbeam_channel as channel;
 
 use driver_rust::elevio::elev::Elevator;
 use driver_rust::elevio::elev::{CAB, HALL_DOWN, HALL_UP};
+
+use log::error;
 
 const NUM_CALL_VARIANTS: usize = 3;
 const NUM_FLOORS: u8 = 4;
 
 pub struct ElevatorDriver {
     elevator: Elevator,
+    thread_sleep_time: u64,
     current_floor: u8,
     is_halted: bool,
     is_obstructed: bool,
@@ -37,6 +42,7 @@ impl ElevatorDriver {
     ) -> ElevatorDriver {
         ElevatorDriver {
             elevator: Elevator::init("localhost:15657", NUM_FLOORS).unwrap(),
+            thread_sleep_time: 1_000,
             current_floor: u8::MAX, // because unknown starting position
             is_halted: false,
             is_obstructed: false,
@@ -53,7 +59,7 @@ impl ElevatorDriver {
         }
     }
 
-    pub fn run(mut self) -> ! {
+    pub fn run(mut self) {
         self.is_obstructed = self.elevator.obstruction();
 
         // reset light on init
@@ -81,26 +87,73 @@ impl ElevatorDriver {
             }
 
             for floor in 0..self.elevator.num_floors {
-                let new_cabin_order = !self.requests[floor as usize][CAB as usize]
+                let new_cabin_order: bool = !self.requests[floor as usize][CAB as usize]
                     && self.elevator.call_button(floor, CAB);
                 if new_cabin_order {
                     self.requests[floor as usize][CAB as usize] = true;
                     let _ = self.hw_request_tx.send((floor, CAB));
                 }
 
-                let new_call_upward = !self.requests[floor as usize][HALL_UP as usize]
+                let new_call_upward: bool = !self.requests[floor as usize][HALL_UP as usize]
                     && self.elevator.call_button(floor, HALL_UP);
                 if new_call_upward {
                     self.requests[floor as usize][HALL_UP as usize] = true;
                     let _ = self.hw_request_tx.send((floor, HALL_UP));
                 }
 
-                let new_call_downward = !self.requests[floor as usize][HALL_DOWN as usize]
+                let new_call_downward: bool = !self.requests[floor as usize][HALL_DOWN as usize]
                     && self.elevator.call_button(floor, HALL_DOWN);
                 if new_call_downward {
                     self.requests[floor as usize][HALL_DOWN as usize] = true;
                     let _ = self.hw_request_tx.send((floor, HALL_DOWN));
                 }
+            }
+
+            // receive updates from state management
+            channel::select! {
+              recv(self.hw_motor_direction_rx) -> msg => {
+                match msg {
+                  Ok(msg) => self.elevator.motor_direction(msg),
+                  Err(error) => {
+                    error!("Failed to set motor direction {}", error);
+                  }
+                }
+              }
+
+              recv(self.hw_button_light_rx) -> msg => {
+                match msg {
+                  Ok(msg) => {
+                    let (floor, call_type, is_lit) = msg;
+                    self.elevator.call_button_light(floor, call_type, is_lit);
+                    self.requests[floor as usize][call_type as usize] = is_lit;
+                  },
+                  Err(error) => {
+                    error!("Failed to set new order or call {}", error);
+                  }
+                }
+              }
+
+              recv(self.hw_floor_indicator_rx) -> msg => {
+                match msg {
+                  Ok(msg) => self.elevator.floor_indicator(msg),
+                  Err(error) => {
+                    error!("Failed to set floor indicator {}", error);
+                  }
+                }
+              }
+
+              recv(self.hw_door_light_rx) -> msg => {
+                match msg {
+                  Ok(msg) => self.elevator.door_light(msg),
+                  Err(error) => {
+                    error!("Failed to set eleavtor doorlight {}", error);
+                  }
+                }
+              }
+              recv(self.terminate_rx) -> _ => {
+                break;
+              }
+              default(Duration::from_millis(self.thread_sleep_time)) => {}
             }
         }
     }
