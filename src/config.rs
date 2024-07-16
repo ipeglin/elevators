f544f88b5a6185cc3881d7692f1d7e7c73d85e4d
ipeglin <philip@eglin.no>
2024-07-16 22:36:45 +0200

    fix: correcting use of serde

--- src/config.rs
+++ src/config.rs
@@ -1,12 +1,12 @@
 use serde::Deserialize;
 use std::fs;
 
-#[destruct(Deserialize)]
+#[derive(Deserialize)]
 pub struct Config {
-    hardware: HardwareConfig,
+    pub hardware: HardwareConfig,
 }
 
-#[destruct(Deserialize)]
+#[derive(Deserialize)]
 pub struct HardwareConfig {
     NUM_FLOORS: u8,
     DRIVER_ADRESS: String,
@@ -16,5 +16,5 @@ pub struct HardwareConfig {
 
 pub fn load() -> Config {
     let config_string = fs::read_to_string("config.toml").expect("Failed to load config file");
-    toml::from_string(config_string).expect("Failed to parse configuration from file")
+    toml::from_str(&config_string).expect("Failed to parse configuration from file")
 }
